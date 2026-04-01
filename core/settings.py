dfdd166bf4c5591f4ad0f4084f9fb66074fab064
Darkio Luft <114611831+DarkioLuft@users.noreply.github.com>
2026-03-31 22:29:02 -0300

    aa

--- core/settings.py
+++ core/settings.py
@@ -11,6 +11,9 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 
 from pathlib import Path
+import os
+STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -25,7 +28,7 @@ SECRET_KEY = 'django-insecure-_m@e(p*-^^h)n(@zj08$%5qv(sz%br(lkk29a@^fh96_q*jb21
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['177.44.248.75', 'localhost', '']
 
 
 # Application definition
