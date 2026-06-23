dcbd81afec39c9fda53a3c70164b209a8abbc289
Darkio Luft <114611831+DarkioLuft@users.noreply.github.com>
2026-06-23 18:21:34 -0300

    update admin.py

--- crud/admin.py
+++ crud/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from .models import User, Receita
 
-# Register your models here.
+
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('id_user', 'name', 'login', 'email', 'status')
+    list_filter = ('status',)
+    search_fields = ('name', 'login', 'email')
+    list_editable = ('status',)
+
+
+@admin.register(Receita)
+class ReceitaAdmin(admin.ModelAdmin):
+    list_display = ('id_receita', 'nome', 'tipo_receita', 'custo', 'user', 'data_criacao')
+    list_filter = ('tipo_receita', 'user')
+    search_fields = ('nome', 'descricao', 'ingredientes')
+    list_per_page = 20
