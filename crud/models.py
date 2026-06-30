ff07fa81bc9e253aafbdef13cfe99484a23c70e3
Darkio Luft <114611831+DarkioLuft@users.noreply.github.com>
2026-06-30 13:10:53 -0300

    Update models.py

--- crud/models.py
+++ crud/models.py
@@ -34,7 +34,7 @@ class Receita(models.Model):
     data_criacao = models.DateTimeField(auto_now_add=True)
     custo = models.DecimalField(max_digits=10, decimal_places=2)
     tipo_receita = models.CharField(max_length=50, choices=TIPO_CHOICES)
-    dicas = models.TextField(blank=True, null=True)
+    #dicas = models.TextField(blank=True, null=True)
 
     def __str__(self):
         return self.nome
