6dae746786a1b9d06b11c85e7cb32ec9be8ef78d
Darkio Luft <114611831+DarkioLuft@users.noreply.github.com>
2026-06-30 13:07:41 -0300

    apresentacao_banco

--- crud/models.py
+++ crud/models.py
@@ -9,6 +9,7 @@ class User(models.Model):
     senha = models.CharField(max_length=128)
     email = models.EmailField(max_length=254, unique=True)
     status = models.BooleanField(default=True)
+    
 
     def __str__(self):
         return self.name
@@ -33,6 +34,7 @@ class Receita(models.Model):
     data_criacao = models.DateTimeField(auto_now_add=True)
     custo = models.DecimalField(max_digits=10, decimal_places=2)
     tipo_receita = models.CharField(max_length=50, choices=TIPO_CHOICES)
+    dicas = models.TextField(blank=True, null=True)
 
     def __str__(self):
         return self.nome
